f74ab3ffa496df1f3b2cb0e6c0462f666e4d3647
Ethan Feng <eyfeng@usc.edu>
2022-03-11 17:48:31 -0800

    setup button on main page

---
+++ app/src/main/java/com/example/uscrecsport/LyonBookingPage.java
@@ -0,0 +1,14 @@
+package com.example.uscrecsport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LyonBookingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lyon_booking_page);
+    }
+}
--- app/src/main/java/com/example/uscrecsport/MainPage.java
+++ app/src/main/java/com/example/uscrecsport/MainPage.java
@@ -2,13 +2,36 @@ package com.example.uscrecsport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        Button lyon = findViewById(R.id.lyon_button);
+        Button village = findViewById(R.id.village_button);
+
+        lyon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPage.this, LyonBookingPage.class);
+                startActivity(intent);
+            }
+        });
+
+        village.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPage.this, VillageBookingPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
