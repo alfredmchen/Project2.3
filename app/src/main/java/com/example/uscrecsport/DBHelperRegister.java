5980a7002c9b3a8208efea3cb59ad498e9e467dc
Ethan Feng <eyfeng@usc.edu>
2022-03-13 19:04:38 -0700

    setup database gym and appointment )

--- app/src/main/java/com/example/uscrecsport/DBHelperRegister.java
+++ app/src/main/java/com/example/uscrecsport/DBHelperRegister.java
@@ -15,7 +15,18 @@ public class DBHelperRegister extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table users(username Text primary key, password Text)");
+        db.execSQL("create table users(username TEXT PRIMARY KEY, password TEXT NOT NULL)");
+        db.execSQL("create table villageGym(time_id INTEGER PRIMARY KEY, time INTEGER NOT NULL)");
+        db.execSQL("create table villageGymAppointment(appointment_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "time_id INTEGER NOT NULL, username TEXT NOT NULL, FOREIGN KEY(time_id) REFERENCES villageGym(time_id))");
+        insertTime(110, 10);
+        insertTime(111, 11);
+        insertTime(112, 12);
+        insertTime(113, 13);
+        insertTime(114, 14);
+        insertTime(115, 15);
+
+
     }
 
     @Override
@@ -23,7 +34,7 @@ public class DBHelperRegister extends SQLiteOpenHelper {
         db.execSQL("drop Table if exists users");
     }
 
-    public boolean insertd(String un, String pw){
+    public boolean insertUser(String un, String pw){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put("username", un);
@@ -39,6 +50,42 @@ public class DBHelperRegister extends SQLiteOpenHelper {
             return true;
         }
     }
+
+    private boolean insertTime(Integer time_id, Integer time){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("time_id", time_id);
+        cv.put("time", time);
+        long res = db.insert("villageGym",null, cv);
+        if(res == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean insertAppointment(Integer time_id, String username){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("username", username);
+        long res = db.insert("villageGym(" + time_id + ")",null, cv);
+        if(res == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean checkAppointment(Integer time_id, String username){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cs = db.rawQuery("select * from villageGynAppointments where username = ?", new String[] {username});
+        if(cs.getCount() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 
     public boolean checkusername(String un){
         SQLiteDatabase db = this.getWritableDatabase();
--- app/src/main/java/com/example/uscrecsport/VillageBookingPage.java
+++ app/src/main/java/com/example/uscrecsport/VillageBookingPage.java
@@ -3,12 +3,16 @@ package com.example.uscrecsport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class VillageBookingPage extends AppCompatActivity {
+    Button mon_10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_village_booking_page);
+
+
     }
 }
