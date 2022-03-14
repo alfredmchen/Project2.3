712f5469ecfa77465befcdce4dbe8c53c862b60d
Alfred Chen <amchen@usc.edu>
2022-03-14 01:45:11 -0700

    updated main page scrollable current appointment textview

--- app/src/main/java/com/example/uscrecsport/MainPage.java
+++ app/src/main/java/com/example/uscrecsport/MainPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,7 +20,9 @@ public class MainPage extends Activity {
         Button lyon = findViewById(R.id.lyon_button);
         Button village = findViewById(R.id.village_button);
         TextView welcome = findViewById(R.id.welcomeusertextview);
+        TextView currentAppt = findViewById(R.id.currentAppointmentTextView);
         welcome.setText("Welcome " + username);
+        currentAppt.setMovementMethod(new ScrollingMovementMethod());
         lyon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
